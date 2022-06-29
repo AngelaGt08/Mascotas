3cc8230ab55eda6cdad3d59bc28366dfd2041850
AngelaGt08 <garzon.giraldo.angela@gmail.com>
2022-06-29 16:51:52 -0500

    Actividades

--- OneDrive - NTT DATA EMEAL/Escritorio/git/Mascotas-main/Mascotas-main/src/test/java/com/example/mascotas/sample/controller/PerrosEntitySample.java
+++ OneDrive - NTT DATA EMEAL/Escritorio/git/Mascotas-main/Mascotas-main/src/test/java/com/example/mascotas/sample/controller/PerrosEntitySample.java
@@ -1,5 +1,22 @@
 package com.example.mascotas.sample.controller;
 
-public class PerrosEntitySample
-{
+import com.example.mascotas.model.PerrosEntity;
+import org.springframework.http.ResponseEntity;
+import java.util.Date;
+public class PerrosEntitySample{
+
+    public static ResponseEntity getPerrosEntity(){
+        PerrosEntity perros = new PerrosEntity(1L,"Rex","Pastor Aleman",4,new Date(), new Date());
+        return ResponseEntity.ok(perros);
+    }
+
+    public static  PerrosEntity getRequestPerrosEntity(){
+        PerrosEntity perros = new PerrosEntity();
+        perros.setNombre("Rex");
+        perros.setRaza("Florez");
+        perros.setEdad(4);
+        perros.setFechaCreacion(new Date());
+        return perros;
+    }
+
 }
--- OneDrive - NTT DATA EMEAL/Escritorio/git/Mascotas-main/Mascotas-main/src/test/java/com/example/mascotas/service/impl/PerrosServiceImplTest.java
+++ OneDrive - NTT DATA EMEAL/Escritorio/git/Mascotas-main/Mascotas-main/src/test/java/com/example/mascotas/service/impl/PerrosServiceImplTest.java
@@ -1,4 +1,38 @@
 package com.example.mascotas.service.impl;
 
+
+import com.example.mascotas.model.PerrosEntity;
+import com.example.mascotas.repository.PerrosRepository;
+import com.example.mascotas.sample.controller.PerrosEntitySample;
+import com.example.mascotas.services.IPerrosServices;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
 public class PerrosServiceImplTest {
+
+    @MockBean
+    PerrosRepository perrosRepository;
+
+    @Autowired
+    IPerrosServices iPerrosServices;
+    private void assertInstanceOf(Class<PerrosEntity> perrosEntityClass, PerrosEntity perros) {
+    }
+    @Test
+    void guardarPerros(){
+        when(perrosRepository.save(any())).thenReturn(PerrosEntitySample.getRequestPerrosEntity());
+        var responseEntity = iPerrosServices.guardarPerros(PerrosEntitySample.getRequestPerrosEntity());
+        PerrosEntity perros = (PerrosEntity) responseEntity.getBody();
+        verify(perrosRepository, times(1)).save(any());
+        assertNotNull(perros);
+        assertEquals("Rex", perros.getNombre());
+        assertInstanceOf(PerrosEntity.class, perros);
+    }
+
 }
